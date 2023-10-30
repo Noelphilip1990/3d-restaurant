72210e2b8653614778dd9b7dec9146bf4899b897
Noelphilip1990 <noyo1000gt@gmail.com>
2023-10-31 00:38:58 +0400

    home link added

--- src/app/nav-heading/nav-heading.component.ts
+++ src/app/nav-heading/nav-heading.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-heading',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavHeadingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  navLink(link: string) {
+    this.router.navigateByUrl(link);
   }
 
 }
