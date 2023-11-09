809d06194b751a9bd8ad3218a863aff60a2fcee7
Noelphilip1990 <noyo1000gt@gmail.com>
2023-11-10 03:11:24 +0400

    add content for 3d in modal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import * as THREE from 'three';
 
+
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -42,5 +45,42 @@ export class DialogContentExampleDialog {
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: { titleName: string }) {
     console.log(data);
+   
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.init();
+  }
+
+  init () {
+    let width = window.innerWidth - 50;
+    let height = window.innerHeight;
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( width, height );
+    let blockCan = document.getElementById('canvas-box');
+    blockCan?.appendChild( renderer.domElement );
+
+    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    camera.position.z = 5;
+
+    function animate() {
+      requestAnimationFrame( animate );
+
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderer.render( scene, camera );
+    }
+
+    animate();
+    
   }
 }
