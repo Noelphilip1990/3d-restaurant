a41c5dd40625520d1ca9277563b36e74bacf558a
Noelphilip1990 <noyo1000gt@gmail.com>
2023-11-10 04:12:11 +0400

    view modal inside width and height adjust

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent, DialogContentExampleDialog } from './home/home.component';
+import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavHeadingComponent } from './nav-heading/nav-heading.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -14,6 +14,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 import { FooterComponent } from './footer/footer.component';
+import { ViewDialogComponent } from './view-dialog/view-dialog.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { FooterComponent } from './footer/footer.component';
     HomeComponent,
     NavHeadingComponent,
     FooterComponent,
+    ViewDialogComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,9 +3,7 @@ import { MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef } from '@angu
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import * as THREE from 'three';
-
-
-
+import { ViewDialogComponent  } from '../view-dialog/view-dialog.component';
 
 @Component({
   selector: 'app-home',
@@ -20,7 +18,7 @@ export class HomeComponent implements OnInit {
   }
 
   openDialog(title: string) {
-    const dialogRef = this.dialog.open(DialogContentExampleDialog, {
+    const dialogRef = this.dialog.open(ViewDialogComponent, {
       height: '100%',
       width: '100%',
       panelClass: 'viewM',
@@ -32,55 +30,4 @@ export class HomeComponent implements OnInit {
     });
   }
 
-}
-
-@Component({
-  selector: 'dialog-content-example-dialog',
-  templateUrl: 'dialog-content-example-dialog.html',
-  styleUrls: ['dialog-content-example-dialog.scss'],
-  standalone: true,
-  imports: [MatDialogModule, MatButtonModule, MatIconModule],
-})
-export class DialogContentExampleDialog {
-
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { titleName: string }) {
-    console.log(data);
-   
-  }
-
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    this.init();
-  }
-
-  init () {
-    let width = window.innerWidth - 50;
-    let height = window.innerHeight;
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize( width, height );
-    let blockCan = document.getElementById('canvas-box');
-    blockCan?.appendChild( renderer.domElement );
-
-    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    const cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-
-    camera.position.z = 5;
-
-    function animate() {
-      requestAnimationFrame( animate );
-
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-
-      renderer.render( scene, camera );
-    }
-
-    animate();
-    
-  }
 }
