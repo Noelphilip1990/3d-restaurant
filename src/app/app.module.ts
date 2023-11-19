945cf2846ad76259482cbb55e76b040af330b153
Noelphilip1990 <noyo1000gt@gmail.com>
2023-11-19 11:20:27 +0400

    add max size initial bundle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import {FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavHeadingComponent } from './nav-heading/nav-heading.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCommonModule } from '@angular/material/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { FooterComponent } from './footer/footer.component';
 import { ViewDialogComponent } from './view-dialog/view-dialog.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
@@ -42,7 +42,6 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     MatGridListModule,
     FormsModule,
     MatMenuModule,
-    ReactiveFormsModule,
   ],
   providers: [MatDialog],
   bootstrap: [AppComponent]
